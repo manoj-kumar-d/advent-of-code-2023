30dce8f606f492fa8deb70aa16d07dbdc25f06c8
Manoj Kumar D <manojkumardmk5@gmail.com>
2023-12-17 15:56:43 +0530

    Day 6 solution for part 2

--- day6/solution.js
+++ day6/solution.js
@@ -35,3 +35,38 @@ fs.readFile('input.txt', (err, data) => {
     console.log(result); //2269432
 
 });
+
+fs.readFile('input_two.txt', (err, data) => {
+    const input = data.toString();
+
+    const lineOne = input.split(os.EOL)[0];
+    const times = lineOne.slice(9, lineOne.length).trim().split(' ')
+        .filter(x => x !== '').map(x => Number(x));
+    const lineTwo = input.split(os.EOL)[1];
+    const distances = lineTwo.slice(9, lineTwo.length).trim().split(' ')
+        .filter(x => x !== '').map(x => Number(x));
+
+    let result = 1;
+    for (let game = 0; game < times.length; game++) {
+        const time = times[game];
+        const recordDistance = distances[game];
+
+        let winningTimes = 0;
+        for (let hold = 0; hold <= time; hold++) {
+            let speed = hold;
+            let travelTime = time - hold;
+
+            let possibleDistance = speed * travelTime;
+            if (possibleDistance > recordDistance) {
+                winningTimes++;
+            }
+        }
+
+        if (winningTimes > 0) {
+            result*=winningTimes;
+        }
+    }
+
+    console.log(result); //35865985
+
+});
